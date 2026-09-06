718b3520e61269130e5cf236d40425be734c903a
EduardoCassin12 <eduardojuniorlopescassin@gmail.com>
2026-09-05 23:20:34 -0300

    Funcionario4 adicionado

--- src/Teste/FuncionarioTest01.java
+++ src/Teste/FuncionarioTest01.java
@@ -30,6 +30,14 @@ import Dominio.Funcionario;
             funcionario3.setDiasDeServico(100);
             funcionario3.calcularSalarioAnual(4);
 
+            Funcionario funcionario4 = new Funcionario();
+            funcionario4.setId("004");
+            funcionario4.setNome("Ana");
+            funcionario4.setCargo("Assistente");
+            funcionario4.setSalario(3500.00);
+            funcionario4.setDiasDeServico(200);
+            funcionario4.calcularSalarioAnual(12);
+
             System.out.println("Informações do Funcionário 1:");
             funcionario1.aumentarSalario(10);
             funcionario1.exibirInformacoes();
@@ -39,6 +47,10 @@ import Dominio.Funcionario;
             System.out.println("\nInformações do Funcionário 3:");
             funcionario3.aumentarSalario(10);
             funcionario3.exibirInformacoes();
+            System.out.println("\nInformações do Funcionário 4:");
+            funcionario4.aumentarSalario(10);
+            funcionario4.exibirInformacoes();
+
         }
     }
 
